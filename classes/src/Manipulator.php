6904ec631001ae4f488854aad4375a2b9e31aed9
MarkBaker <mark@lange.demon.co.uk>
2020-12-23 14:03:22 +0100

    PHPCS cleanup

--- classes/src/Manipulator.php
+++ classes/src/Manipulator.php
@@ -25,13 +25,15 @@ class Manipulator
             ->bindTo(null, $className)($className, ...$constructorArguments);
     }
 
-    private function executor() {
-        return function($instance, string $methodName, ...$methodArguments) {
+    private function executor()
+    {
+        return function ($instance, string $methodName, ...$methodArguments) {
             return $instance->$methodName(...$methodArguments);
         };
     }
 
-    public function execute($instance, string $methodName, ...$methodArguments) {
+    public function execute($instance, string $methodName, ...$methodArguments)
+    {
         if (!is_object($instance)) {
             throw new Exception(sprintf('The first argument passed to execute must be an object'));
         } elseif ((new \ReflectionClass(get_class($instance)))->isInternal()) {
